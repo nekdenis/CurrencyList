771c72bee1488924b98ffceb0e6e690c9c6b594b
nekdenis <nekdenis@gmail.com>
2014-12-28 22:57:46 +0300

    Added notifications

--- app/src/main/java/com/github/nekdenis/currencylist/util/Constants.java
+++ app/src/main/java/com/github/nekdenis/currencylist/util/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
 
     public static final int LOADER_ID_DETAIL = 0;
     public static final int LOADER_ID_RATES = LOADER_ID_DETAIL + 1;
+
+    public static final String PREFERENCES_LAST_NOTIFICATION_KEY = "PREFERENCES_LAST_NOTIFICATION_KEY";
 }
---
+++ app/src/main/java/com/github/nekdenis/currencylist/util/NotificationUtil.java
@@ -0,0 +1,56 @@
+package com.github.nekdenis.currencylist.util;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
+
+import com.github.nekdenis.currencylist.R;
+import com.github.nekdenis.currencylist.activity.MainActivity;
+
+public class NotificationUtil {
+
+    public static final int DAY_IN_MILLIS = 24 * 60 * 60 * 1000;
+    private static final int WEATHER_NOTIFICATION_ID = 11;
+
+    public static void showNewUpdateNotification(Context context, String notificatonMessage) {
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+
+        long lastSync = prefs.getLong(Constants.PREFERENCES_LAST_NOTIFICATION_KEY, 0);
+
+        if (System.currentTimeMillis() - lastSync >= DAY_IN_MILLIS) {
+
+            String title = context.getString(R.string.app_name);
+
+            NotificationCompat.Builder mBuilder =
+                    new NotificationCompat.Builder(context)
+//                                .setSmallIcon(iconId)
+                            .setContentTitle(title)
+                            .setContentText(notificatonMessage);
+
+            Intent resultIntent = new Intent(context, MainActivity.class);
+
+            TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+            stackBuilder.addNextIntent(resultIntent);
+            PendingIntent resultPendingIntent =
+                    stackBuilder.getPendingIntent(
+                            0,
+                            PendingIntent.FLAG_UPDATE_CURRENT
+                    );
+            mBuilder.setContentIntent(resultPendingIntent);
+
+            NotificationManager mNotificationManager =
+                    (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+            mNotificationManager.notify(WEATHER_NOTIFICATION_ID, mBuilder.build());
+
+
+            SharedPreferences.Editor editor = prefs.edit();
+            editor.putLong(Constants.PREFERENCES_LAST_NOTIFICATION_KEY, System.currentTimeMillis());
+            editor.commit();
+        }
+    }
+}
